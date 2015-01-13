2116e5bb2093b40f3f81414122138ea1684b8892
menyet <info@mandli.sk>
2015-01-13 21:55:28 +0100

    first commit

---
+++ Gotham.WebApp/js/app.js
@@ -0,0 +1,21 @@
+﻿var app = Ember.Application.create();
+
+app.Router.map(function () {
+    // put your routes here
+});
+
+app.IndexRoute = Ember.Route.extend({
+    model: function () {
+        //return ['red', 'yellow', 'blue', 'green'];
+
+        var artistObjects = [];
+        Ember.$.getJSON('http://localhost:50050/artists', function (artists) {
+            artists.forEach(function (data) {
+                artistObjects.pushObject(App.Artist.createRecord(data));
+            });
+        });
+        return artistObjects;
+
+
+    }
+});
